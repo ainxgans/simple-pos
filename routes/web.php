4da1743b8d441a1f2d33c378e4d7083b06c385ee
Maulana <maulana.120100@gmail.com>
2024-11-08 00:45:36 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Middleware\CheckLoginMiddleware;
+use App\Livewire\Cart;
+use App\Livewire\CreateCategory;
+use App\Livewire\CreateProduct;
+use App\Livewire\Home;
+use App\Livewire\Login;
+use App\Livewire\PaymentSuccess;
 use Illuminate\Support\Facades\Route;
-Route::middleware([\App\Http\Middleware\CheckLoginMiddleware::class])->group(function () {
-    Route::get('/', \App\Livewire\Home::class)->name('home');
-    Route::get('/category',\App\Livewire\CreateCategory::class)->name('category');
-    Route::get('/product', \App\Livewire\CreateProduct::class)->name('product');
-    Route::get('/cart', \App\Livewire\Cart::class)->name('cart');
-    Route::get('/payment', \App\Livewire\PaymentSuccess::class)->name('payment');
+Route::middleware([CheckLoginMiddleware::class])->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('/category', CreateCategory::class)->name('category');
+    Route::get('/product', CreateProduct::class)->name('product');
+    Route::get('/cart', Cart::class)->name('cart');
+    Route::get('/payment', PaymentSuccess::class)->name('payment');
 });
 
-Route::get('/login', \App\Livewire\Login::class)->name('login');
+Route::get('/login', Login::class)->name('login');
 Route::get('/logout', function () {
     Auth::logout();
     return redirect()->route('login');
